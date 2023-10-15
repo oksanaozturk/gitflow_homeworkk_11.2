452ea173f1940bc654173e09abad64bee9a8c576
Oksana Ozturk <oksanaozturk@gmail.com>
2023-10-15 23:48:39 +0300

    Add str_func.py

---
+++ str_func.py
@@ -0,0 +1,7 @@
+def converts_to_capital_letters(text):
+    new_text = text.title()
+    return new_text
+
+if __name__ == "__main__":
+    text = "мама моет раму."
+    print(converts_to_capital_letters(text))
