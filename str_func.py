c66841477b379ceb8e5ef6230fa7da9c20f8670d
Oksana Ozturk <oksanaozturk@gmail.com>
2023-10-16 11:36:57 +0300

    Update tr_func.py (удаление кода проверки функции)

--- str_func.py
+++ str_func.py
@@ -1,7 +1,3 @@
 def converts_to_capital_letters(text):
     new_text = text.title()
     return new_text
-
-if __name__ == "__main__":
-    text = "мама моет раму."
-    print(converts_to_capital_letters(text))
