30063265f148df61e6fb10859affca98da2efef1
Oksana Ozturk <oksanaozturk@gmail.com>
2023-10-16 11:54:28 +0300

    Add converts_all_letters_uppercase

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def converts_to_capital_letters(text):
     new_text = text.title()
     return new_text
+
+
+def converts_all_letters_uppercase(text):
+    new_text_1 = text.upper()
+    return new_text_1
