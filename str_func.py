6f6f2895cc1a8e3d3d2b8efb8f764a329dc9da86
Oksana Ozturk <oksanaozturk@gmail.com>
2023-10-16 11:57:12 +0300

    Add docstring to converts_all_letters_uppercase

--- str_func.py
+++ str_func.py
@@ -4,5 +4,6 @@ def converts_to_capital_letters(text):
 
 
 def converts_all_letters_uppercase(text):
+    """Функцмя выводящая все слова в верхнем регистре"""
     new_text_1 = text.upper()
     return new_text_1
